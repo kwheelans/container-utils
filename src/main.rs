a55c81eb57c9021b9caddbf2da8c16d73457e471
Kevin Wheelans <kevin.wheelans@proton.me>
2026-01-16 00:12:40 -0500

    use cli

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use crate::download::download_css_archive;
-use std::process::ExitCode;
+use clap::Parser;
 use tracing::error;
 use tracing::level_filters::LevelFilter;
+use crate::cli::CliArgs;
 
 mod download;
 mod cli;
 
-fn main() -> ExitCode {
+fn main() {
     tracing_subscriber::fmt()
         .with_max_level(LevelFilter::INFO)
         .init();
+    let cli = CliArgs::parse();
 
-    match download_css_archive() {
-        Ok(_) => ExitCode::SUCCESS,
-        Err(error) => {
-            error!("{}", error);
-            ExitCode::FAILURE
+    if cli.pico_css_download {
+        if let Err(error) = download_css_archive() {
+            error!("{}", error)
         }
     }
 }
